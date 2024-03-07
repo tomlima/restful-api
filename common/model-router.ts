01d260e03dc7b129d64d0b5a5c0336f11f63cf3a
Tom lima <tom@tomlima.me>
2024-03-07 08:11:29 -0300

    Refactor route model

---
+++ common/model-router.ts
@@ -0,0 +1,72 @@
+import { Router } from "./router";
+import * as mongoose from 'mongoose'
+import { NotFoundError } from "restify-errors";
+
+export abstract class ModelRouter<D extends mongoose.Document> extends Router{
+    constructor(protected model: mongoose.Model<D>){
+        super()
+    }
+
+    validateId = (req,resp,next) => {
+        if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+            next(new NotFoundError('Document not found'))
+        }else{
+            next()
+        }
+    } 
+
+    findAll =  async (req,resp,next) => { 
+        try{
+            const users = await this.model.find()
+            resp.json(users)
+            return next()
+        }catch(error){
+            next(error)
+        }
+    }
+
+    findById = async (req,resp,next) => {
+        try{
+            const user = await this.model.findById(req.params.id)
+            resp.json(user)
+            return next()
+        }catch(error){
+            next(error)
+        }
+    }
+
+    save =  async (req, resp, next) => {
+        try{
+            let user = new this.model(req.body);  
+            const result = await user.save() 
+            resp.json(result)
+            return next() 
+        }catch(error){
+            next(error)
+        }
+    }
+
+    update = async(req,resp,next) => {
+        const options = {runValidators: true}
+        try{  
+            const result = await this.model.findByIdAndUpdate({_id:req.params.id}, req.body, options)
+            resp.json(result)
+            return next()
+        }catch(err){
+             next(err)
+        }	  
+    }
+
+    delete =  async (req,resp,next) => {
+        try{
+            await this.model.deleteOne({_id: `${req.params.id}`})
+            resp.send(204)
+          }catch(err){
+            if(err.kind === 'ObjectId'){
+                resp.send(404)
+              }
+            resp.send(500)
+          }
+        return next()
+    }
+}
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,79 +1,18 @@
 import { Server } from 'restify'
-import {Router} from '../common/router'
+import {ModelRouter} from '../common/model-router'
 import {User} from './users.model'
 
-class UsersRouter extends Router {
+class UsersRouter extends ModelRouter<User> {
+  constructor(){
+    super(User)
+  }
+
   applyRoutes(application: Server) {
-      /*------------------
-      Getting all users
-      ------------------*/
-      application.get('/users', async (req,resp,next) => { 
-          try{
-              const users = await User.find()
-              resp.json(users)
-              return next()
-            }catch(error){
-              next(error)
-            }
-        })
-      
-      /*--------------------
-      Getting a single user
-      --------------------*/
-      application.get("/users/:id", async (req,resp,next) => {
-          try{
-              const user = await User.findById(req.params.id)
-              resp.json(user)
-              return next()
-            }catch(error){
-              next(error)
-            }
-        })
-      
-      /*------------------
-      Create a new user
-      ------------------*/
-      application.post("/users", async (req, resp, next) => {
-          try{
-              let user = new User(req.body);  
-              const result = await user.save() 
-              result.password  = undefined
-              resp.json(result)
-              return next() 
-            }catch(error){
-              next(error)
-            }
-        })
-      
-      /*------------------
-      Update a single user
-      ------------------*/
-      application.put("/users/:id",async(req,resp,next) => {
-        const options = {runValidators: true}
-        try{  
-          const result = await User.findByIdAndUpdate({_id:req.params.id}, req.body, options)
-          resp.json(result)
-          return next()
-        }catch(err){
-          next(err)
-        }	  
-        })
-      
-      /*------------------
-      Delete a single user
-      ------------------*/
-      application.del("/users/:id", async (req,resp,next) => {
-          try{
-              await User.deleteOne({_id: `${req.params.id}`})
-              resp.send(204)
-            }catch(err){
-              if(err.kind === 'ObjectId'){
-                  resp.send(404)
-                }
-              resp.send(500)
-            }
-          return next()
-        })
+      application.get('/users', this.findAll)
+      application.get("/users/:id", [this.validateId,this.findById])
+      application.post("/users", this.save)
+      application.put("/users/:id", [this.validateId,this.update])
+      application.del("/users/:id", [this.validateId,this.delete])
     }
 }
 
