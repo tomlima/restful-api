12a71825b0b583d96e281e5ce1d8ff9489024444
Tom lima <tom@tomlima.me>
2024-03-21 19:35:05 -0300

    Add a notfound user case test

--- users/users.test.ts
+++ users/users.test.ts
@@ -23,7 +23,6 @@ test('Get /users', async ()=>{
     }catch(error){
         throw new Error(error)
     }
-
 })
 
 
@@ -35,6 +34,30 @@ test('Post /users', async () => {
     }) 
     expect(response.status).toBe(200)
     expect(response.body._id).toBeDefined()
+})
+
+test('Get /users/:id - not found', async () => {
+    try{
+        const response = await request('http://localhost:3001').get('/users/aaaaaa');
+        expect(response.status).toBe(404)
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+test('Update /users/:id', async ()=>{
+    const postResponse = await request('http://localhost:3001').post('/users').send({
+        name: 'user2',
+        email: 'user2@email.com',
+        password: '123456'
+    }) 
+    const updateResponse = await request('http://localhost:3001').put(`/users/${postResponse.body._id}`).send({
+        name: 'user2 - put',
+        email: 'user2@email.com',
+        password: '123456'
+    }) 
+    expect(updateResponse.status).toBe(200)
+    expect(updateResponse.body._id).toBeDefined()
 })
 
 afterAll(()=>{
